50c350fb729446a4b1f49e7049ecae1f572285ee
land-pack <landpack@sina.com>
2018-07-24 14:36:08 +0800

    fixed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bitcoin-port
 API For Bitcoin Write By Flask
+
+
+
+# ./bitcoind -testnet -rpcbind=0.0.0.0:8332 -server -rest=1
+
--- example/app.py
+++ example/app.py
@@ -91,14 +91,15 @@ def api_tx_signrawtransaction():
     address = body.get('address')
     scriptPubKey = body.get('scriptPubKey')
     redeemScript = body.get('redeemScript')
+    sighashtype = body.get('sighashtype') if body.get('sighashtype') else 'ALL'
     dumpprivkey = body.get('dumpprivkey') if body.get('dumpprivkey') else rpc_connection.dumpprivkey(address)
     raw_tx_hex = body.get('raw_tx_hex') if body.get('raw_tx_hex') else rpc_connection.createrawtransaction(
             [{"txid":txid, "vout":vout}], {address: amount}) # 256 absurdly-high-fee
     d = rpc_connection.signrawtransaction(raw_tx_hex, [{"txid":txid, "vout":vout, "scriptPubKey":scriptPubKey, 
-            "redeemScript":redeemScript}], [dumpprivkey]) # 256 absurdly-high-fee
+            "redeemScript":redeemScript}], [dumpprivkey], sighashtype) # 256 absurdly-high-fee
     return render_json(d)
 
-@app.route("/api/tx/sendrawtransaction", methods=['POST'])
+@app.route("/api/v1/tx/sendrawtransaction", methods=['POST'])
 def api_tx_sendrawtransaction():
     """
 	{
@@ -133,6 +134,78 @@ def api_tx_sendrawtransaction():
 
 
 
+
+@app.route("/api/v2/tx/sendrawtransaction", methods=['POST'])
+def api_tx_sendrawtransaction_safe():
+    """
+	{
+	  "txid":"ab9d02994fbca2f577c8deaa224e82dbfa3b3c5ecf3d3f0fd63aaff1c4f3c28c",
+	  "vout":1,
+	  "amount":0.8,
+	  "address":"2MwkxLxbgdZKGEN4fS8GQATqQGXyRpFbkuf",
+	  "scriptPubKey": "a914317fec65edbff6e4350fbe668cfedaf54ea9061587",
+	  "redeemScript": "0014c21bbdc4a642e391a6401bf30ac3e4e8e9669ea2"
+	}
+    """
+    body = request.get_json(force=True)
+    txid = body.get('txid')
+    vout = body.get('vout')
+    amount = body.get('amount')
+    address = body.get('address')
+    scriptPubKey = body.get('scriptPubKey')
+    redeemScript = body.get('redeemScript')
+    dumpprivkey = body.get('dumpprivkey') if body.get('dumpprivkey') else rpc_connection.dumpprivkey(address)
+    raw_tx_hex = body.get('raw_tx_hex') if body.get('raw_tx_hex') else rpc_connection.createrawtransaction(
+            [{"txid":txid, "vout":vout}], {address: amount}) # 256 absurdly-high-fee
+            #[{"txid":txid, "vout":vout}], {address: amount, address2: amount of rest balance}) # 256 absurdly-high-fee
+            
+    hex_object = rpc_connection.signrawtransaction(raw_tx_hex, [{"txid":txid, "vout":vout, "scriptPubKey":scriptPubKey, 
+            "redeemScript":redeemScript, "amount":amount}], [dumpprivkey]) # 256 absurdly-high-fee
+    hex_code = hex_object.get("hex")
+    try:
+        d = rpc_connection.sendrawtransaction(hex_code)
+    except:
+        d = {
+            "traceback": traceback.format_exc()
+        }
+    return render_json(d)
+
+
+@app.route("/api/v3/tx/sendrawtransaction", methods=['POST'])
+def api_tx_v3_sendrawtransaction_safe():
+    """
+	{
+	  "txid":"ab9d02994fbca2f577c8deaa224e82dbfa3b3c5ecf3d3f0fd63aaff1c4f3c28c",
+	  "vout":1,
+	  "amount":0.8,
+	  "address":"2MwkxLxbgdZKGEN4fS8GQATqQGXyRpFbkuf",
+	  "scriptPubKey": "a914317fec65edbff6e4350fbe668cfedaf54ea9061587",
+	  "redeemScript": "0014c21bbdc4a642e391a6401bf30ac3e4e8e9669ea2"
+	}
+    """
+    body = request.get_json(force=True)
+    txid_generate_by = body.get('method') or 'sendfrom'
+
+    txid = rpc_connection.sendfrom(fromaccount, toaddress, amount)
+    vout = body.get('vout')
+    amount = body.get('amount')
+    address = body.get('address')
+    scriptPubKey = body.get('scriptPubKey')
+    redeemScript = body.get('redeemScript')
+
+    dumpprivkey =  rpc_connection.dumpprivkey(address)
+    raw_tx_hex = rpc_connection.createrawtransaction([{"txid":txid, "vout":vout}], {address: amount}) # 256 absurdly-high-fee
+            
+    hex_object = rpc_connection.signrawtransaction(raw_tx_hex, [{"txid":txid, "vout":vout, "scriptPubKey":scriptPubKey, 
+            "redeemScript":redeemScript, "amount":amount}], [dumpprivkey]) # 256 absurdly-high-fee
+    hex_code = hex_object.get("hex")
+    try:
+        d = rpc_connection.sendrawtransaction(hex_code)
+    except:
+        d = {
+            "traceback": traceback.format_exc()
+        }
+    return render_json(d)
 
 
 
